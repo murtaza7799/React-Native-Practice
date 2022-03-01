6e544a5092518027a1cd7e3b66dce8be72b35f81
murtaza7799 <murtazasarwar@live.com>
2022-03-01 23:48:19 +0500

    master

---
+++ Components/login.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+
+function login(props) {
+    return (
+   <Text>Test</Text>
+    );
+}
+
+export default login;
